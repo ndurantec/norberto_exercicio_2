b266964de75f63030c78d8a8dcd1bbdbbd095bc1
ndurantec <126429791+ndurantec@users.noreply.github.com>
2025-03-17 07:45:27 -0300

    aula de ti do professor Norberto

---
+++ norberto/script.js
@@ -0,0 +1,17 @@
+var primeiraNota = prompt("Digite a primeira nota");
+
+
+
+
+
+
+
+
+
+
+alert("Você digitou muita coisa se arrependeu e apagou");
+
+
+alert( primeiraNota  + 1   );
+
+console.log()
